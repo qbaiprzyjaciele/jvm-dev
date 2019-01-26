1bdcb31174f7ffc274a6ee82a2133353dc818884
Jakub Smorąg <qbaiprzyjaciele@gmail.com>
2019-01-26 03:59:05 +0100

    loading basic class info

--- class_load.c
+++ class_load.c
@@ -1,3 +1,9 @@
+/**
+ * @author Jakub Smorąg
+ */
+
+
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "constant_field_list.h"
@@ -106,18 +112,90 @@ typedef struct field_info
 	attribute_info * attributes;
 } field_info;
 
+typedef struct simple_struct 
+{
+	char field;
+	int * ptr;
+} simple_struct;
+
+int big_endian_int(char * bytes) 
+{
+	//TODO
+	return 1;
+}	
+
+unsigned short big_endian_unsigned_short(char bytes)
+{
+	//TODO
+	return 1;
+}
+
+
 
 int main()
 {
 	void * constant_pool = (void*)malloc(4);
 	char bytes[4] = {'a','b','a','a'};
+	FILE * classFile;
+	char cafebabe[4];
+	unsigned short minor_version;
+	unsigned short major_version;
+	unsigned short constant_pool_count;
+	
 	constant_pool = bytes;
 	CONSTANT_MethodHandle_info * info = (CONSTANT_MethodHandle_info*)constant_pool;
 	printf("info->tag=%c\n",info->tag);
 	printf("info->reference_kind=%c\n",info->reference_kind);
 	printf("info->reference_index=%d\n",info->reference_index);
+	int cp_size = 10;
+	printf("::creating_list...\n");
+	printf("sizeof(cfl)=%d\n",(int)sizeof(constant_field_list));
+	void * pointer = 0;
+	printf("poiner: %d\n", (int)sizeof(pointer));
+	char one_byte = 0;
+	printf("sizeof(one_byte)=%d\n", (int)sizeof(one_byte));
+	constant_field_list * cp_array = (constant_field_list*) malloc((sizeof(constant_field_list))*cp_size);
+	simple_struct * ss = (simple_struct*)malloc((sizeof(simple_struct)));
+	ss->field = 'q';
+	int val = 123;
+	ss->ptr = &val;
+
+	cp_array[0].tag = 'a';
+	cp_array[0].data = (void*)ss;
+	
+	classFile = fopen("Example2.class","rb");
+	fread(cafebabe,sizeof(char)*4,1,classFile);
+	fread(&minor_version,sizeof(unsigned short),1,classFile);
+	fread(&major_version,sizeof(unsigned short),1,classFile);
+
+	printf("cafebabe[0] = %d",(int)cafebabe[0]);
+	printf("cafebabe[1] = %d",(int)cafebabe[1]);
+	printf("cafebabe[2] = %d",(int)cafebabe[2]);
+	printf("cafebabe[3] = %d",(int)cafebabe[3]);
+
+	if(((unsigned short)cafebabe[0]) == 0xfe)
+	{
+		printf("cafebabe[0] == 0xfe\n");
+	}
+
+	if((unsigned short)cafebabe[0] == 0xca)
+	{
+		printf("cafebabe[0] == 0xca\n");
+	}		
+
+	printf("minor_version = %d, major_version = %d", minor_version, major_version);
+	if(major_version == 0x33)
+	{
+		printf("major_version == 0x0033\n");
+	}
+	printf("::list created\n");
+
+	free(cp_array);
+	fclose(classFile);
 	return 0;
-}		
+}
+
+
 
 
 
